f4b03f2445e670e4a3441dec06eb22868c2c1142
SambhabBose <bosesambhab2020@gmail.com>
2025-07-30 00:07:50 +0530

    understand react component basics

--- 01reactbasics/src/App.js
+++ 01reactbasics/src/App.js
@@ -1,7 +1,12 @@
+import Para from "./Para";
 
 function App() {
   return (
+
+    <>
     <h1>Hello, React!</h1>
+    <Para />
+    </>
   );
 }
 
---
+++ 01reactbasics/src/Para.js
@@ -0,0 +1,10 @@
+function Para(){
+    return (
+        <>
+        <h2>This is A Heading</h2>
+        <p>This is A Paragraph</p>
+        </>
+    )
+}
+
+export default Para;
